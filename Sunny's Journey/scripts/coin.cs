c9ba18e40a016c4d3efca312a8385c3f6a63d589
Liam Webb <lwebb2022@gmail.com>
2024-07-09 11:55:08 +1000

    Coin removed upon player interaction

--- Sunny's Journey/scripts/coin.cs
+++ Sunny's Journey/scripts/coin.cs
@@ -3,14 +3,11 @@ using System;
 
 public partial class coin : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void _on_body_entered(Node2D body)
 	{
-		GD.Print("Im a coin");
+	GD.Print("+1 Coin");
+	QueueFree();
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	
 }
+
