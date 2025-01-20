8a041205e1148dda746d16420e0564da42cc2f1d
Liam Webb <lwebb2022@gmail.com>
2025-01-20 19:06:25 +1100

    Added background, and improved kill animation/effect

--- Sunny's Journey/scripts/killzone.cs
+++ Sunny's Journey/scripts/killzone.cs
@@ -15,11 +15,14 @@ public partial class killzone : Area2D
 	private void _on_body_entered(Node2D body)
 	{
 		GD.Print("You died");
+		Engine.TimeScale = 0.5;
+		body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
 		_timer.Start();
 	}
 	
 	private void _on_timer_timeout()
 	{
+		Engine.TimeScale = 1;
 		GetTree().ReloadCurrentScene();
 	}
 }
